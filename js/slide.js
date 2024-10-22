eadaa39dc7a61b105d6dc5be7736e7e386143303
wellington <wellingtoncarlos.tech@gmail.com>
2024-10-21 23:35:33 -0300

    aperfeiçoamento slide

---
+++ js/slide.js
@@ -0,0 +1,42 @@
+function borboleta() {
+    document.querySelector('.slide').style.background = 'url(/imagens/borboleta)'
+    document.querySelector('.slide').style.backgroundRepeat = 'no-repeat'
+    document.querySelector('.slide').style.backgroundSize = 'cover'
+    document.querySelector('.slide').style.objectFit= 'cover'
+    document.querySelector('.slide').style.backgroundPosition = 'center'
+    document.querySelector('.slide').style.transition = 'background 2.8s ease, transform 2.8s ease';
+    document.querySelector('.slide').style.transform = 'rotateY(150deg)'
+
+
+    
+}
+function lago() {
+    document.querySelector('.slide').style.background = 'url(/imagens/lago.jpg)'
+    document.querySelector('.slide').style.backgroundRepeat = 'no-repeat'
+    document.querySelector('.slide').style.backgroundSize = 'cover'
+    document.querySelector('.slide').style.backgroundPosition = 'center'
+     document.querySelector('.slide').style.transition = 'background 2.8s ease, transform 2.8s ease';
+    document.querySelector('.slide').style.transform = 'rotateY(150deg)'
+    
+}
+function sertao() {
+    document.querySelector('.slide').style.background = 'url(/imagens/sertão.png)'
+    document.querySelector('.slide').style.backgroundRepeat = 'no-repeat'
+    document.querySelector('.slide').style.backgroundSize = 'cover'
+    document.querySelector('.slide').style.backgroundPosition = 'center'
+    document.querySelector('.slide').style.transition = 'background 2.8s ease';
+}
+function sertao__2() {
+    document.querySelector('.slide').style.background = 'url(/imagens/sertão__2.jpeg)'
+    document.querySelector('.slide').style.backgroundRepeat = 'no-repeat'
+    document.querySelector('.slide').style.backgroundSize = 'cover'
+    document.querySelector('.slide').style.backgroundPosition = 'center'
+    document.querySelector('.slide').style.transition = 'background 2.8s ease';
+}
+function vaqueiros() {
+    document.querySelector('.slide').style.background = 'url(/imagens/vaqueiros.jpeg)'
+    document.querySelector('.slide').style.backgroundRepeat = 'no-repeat'
+    document.querySelector('.slide').style.backgroundSize = 'cover'
+    document.querySelector('.slide').style.backgroundPosition = 'center'
+    document.querySelector('.slide').style.transition = 'background 2.8s ease';
+}
